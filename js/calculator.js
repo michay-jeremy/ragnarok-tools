0e176982a49c2526f688834ed1a556fbc2548411
michay.j.jm <michay.j.jm@gmail.com>
2023-09-09 16:39:18 +0200

    add js stats button (str)

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,19 @@
+const baseLevelSelect = document.getElementById('base-level');
 const jobClassSelect = document.getElementById('classe-select');
 const characterImage = document.querySelector('.calculator > .calculator-character > .character > img');
 const genderRadios = document.querySelectorAll('.gender input[name=gender-name]');
+const statsButtonAdd = document.getElementById('strength_button-add');
+const statsButtonDel = document.getElementById('strength_button-del');
+const statsButtonNumber = document.getElementById('strength_stats-point');
+let currentBaseLevel = '1';
 let currentGender = 'male';
 let currentJobClass = 'novice';
+let currentStats = parseInt(statsButtonNumber.innerText);
+
+baseLevelSelect.addEventListener('change', function (e) {
+    currentBaseLevel = e.target.value++;
+    console.log(e);
+})
 
 const JOB_GENDER_EXCEPTIONS = {
     female: {
@@ -43,4 +54,24 @@ jobClassSelect.addEventListener('change', function (e) {
     currentJobClass = e.target.value;
     setJobException(JOB_GENDER_EXCEPTIONS, currentJobClass, currentGender);
     characterImage.setAttribute('src', `../../img/sprites/${currentJobClass}-${currentGender}-removebg.png`);
+});
+
+statsButtonAdd.addEventListener('click', function (e) {
+    if (currentStats<99) {
+        currentStats = currentStats+1;
+        statsButtonNumber.innerHTML = currentStats;
+    }
+    else {
+        document.getElementById('strength_button-add').disabled=true;
+    }
+});
+
+statsButtonDel.addEventListener('click', function (e) {
+    if (currentStats>0) {
+        currentStats = currentStats-1;
+        statsButtonNumber.innerHTML = currentStats;
+    }
+    else {
+        document.getElementById('strength_button-del').disabled=true;
+    }
 });
